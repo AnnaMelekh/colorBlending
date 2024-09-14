5023e375ffae5e1d934f2104128477bd6c06608d
AnnaMelekh <annasmelekhina@gmail.com>
2024-09-14 20:02:32 +0200

    colorPicker test

--- colorBlending/ViewController.swift
+++ colorBlending/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController, UIColorPickerViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let button = UIButton (frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        view.addSubview(button)
+        button.backgroundColor = .systemGreen
+        button.setTitle("Select color", for: .normal)
+        button.center = view.center
+        button.addTarget(self, action: #selector(didTapSelectColor), for: .touchUpInside)
     }
-
-
+    @objc private func didTapSelectColor() {
+        let colorPickerVC = UIColorPickerViewController()
+        colorPickerVC.delegate = self
+        colorPickerVC.supportsAlpha = false
+        present(colorPickerVC, animated: true)
+    }
+    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
+        let _ = viewController.selectedColor
+    }
+    
+    func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
+        let color = viewController.selectedColor
+        view.backgroundColor = color
+    }
 }
 
