524363879f7c55e8edb070c4c220a3d5317e8f36
AnnaMelekh <annasmelekhina@gmail.com>
2025-01-20 22:46:55 +0100

    added func - getRed, mixColors for resultView

--- colorBlending/ViewController.swift
+++ colorBlending/ViewController.swift
@@ -7,22 +7,68 @@
 
 import UIKit
 
-final class ViewController: UIViewController, UIColorPickerViewControllerDelegate {
+var firstColorButton = UIButton()
+var secondColorButton = UIButton()
+private var selectedButton: UIButton?
+private var color1: UIColor?
 
+private var color2: UIColor?
+private var colorResult: UIColor?
+
+var resultView: UIView = {
+    let resultView = UIButton()
+    resultView.backgroundColor = .white
+    resultView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+        resultView.widthAnchor.constraint(equalToConstant: 90),
+        resultView.heightAnchor.constraint(equalToConstant: 90),
+    ])
+    resultView.layer.borderColor = UIColor.black.cgColor
+    resultView.layer.borderWidth = 0.5
+    
+    return resultView
+}()
+
+final class ViewController: UIViewController, UIColorPickerViewControllerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let button = UIButton (frame: CGRect(x: 0, y: 0, width: 200, height: 50))
-        view.addSubview(button)
-        button.backgroundColor = .systemGreen
-        button.setTitle("Select color", for: .normal)
-        button.center = view.center
-        button.addTarget(self, action: #selector(didTapSelectColor), for: .touchUpInside)
+        
+        setupUI()
+        
+        firstColorButton.addTarget(self, action: #selector(didTapSelectColor(_:)), for: .touchUpInside)
+        secondColorButton.addTarget(self, action: #selector(didTapSelectColor(_:)), for: .touchUpInside)
+        
     }
-    @objc private func didTapSelectColor() {
+    
+    func setupUI() {
+        
+        firstColorButton = setupColorButton()
+        secondColorButton = setupColorButton()
+        
+        view.addSubview(firstColorButton)
+        view.addSubview(secondColorButton)
+        view.addSubview(resultView)
+        
+        NSLayoutConstraint.activate([
+            firstColorButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            firstColorButton.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 300),
+            secondColorButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            secondColorButton.centerYAnchor.constraint(equalTo: firstColorButton.bottomAnchor, constant: 150),
+            
+            resultView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            resultView.centerYAnchor.constraint(equalTo: secondColorButton.bottomAnchor, constant: 150),
+            
+        ])
+        
+    }
+    
+    @objc private func didTapSelectColor(_ sender: UIButton) {
         let colorPickerVC = UIColorPickerViewController()
         colorPickerVC.delegate = self
         colorPickerVC.supportsAlpha = false
+        selectedButton = sender
         present(colorPickerVC, animated: true)
     }
     func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
@@ -30,8 +76,73 @@ final class ViewController: UIViewController, UIColorPickerViewControllerDelegat
     }
     
     func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
-        let color = viewController.selectedColor
-        view.backgroundColor = color
+        var color = viewController.selectedColor
+        selectedButton?.backgroundColor = color
+        
+        if selectedButton === firstColorButton {
+            color1 = color
+        } else if selectedButton === secondColorButton {
+            color2 = color
+        }
+        
+        resultView.backgroundColor = mixColors()
+
+        dismiss(animated: true)
+        
+    }
+    
+    private func getColors(color: UIColor?) -> (red: Int, green: Int, blue: Int)? {
+        guard let color = color else { return nil }
+            var red: CGFloat = 0
+            var green: CGFloat = 0
+            var blue: CGFloat = 0
+            var alpha: CGFloat = 0
+        
+            if color.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+                let redValue = Int(red * 255)
+                let greenValue = Int(green * 255)
+                let blueValue = Int(blue * 255)
+                return (redValue, greenValue, blueValue)
+            } else {
+                print("error with colors")
+                return nil
+            }
+        
+    }
+    
+    private func mixColors() -> UIColor? {
+        guard let color1Components = getColors(color: color1),
+              let color2Components = getColors(color: color2) else {
+            print("no colors")
+            return nil
+        }
+        
+        let red = (color1Components.red + color2Components.red) / 2
+        let green = (color1Components.green + color2Components.green) / 2
+        let blue = (color1Components.blue + color2Components.blue) / 2
+        
+        return UIColor(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: 1
+        )
+    }
+    
+    private func setupColorButton() -> UIButton {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.widthAnchor.constraint(equalToConstant: 90),
+            button.heightAnchor.constraint(equalToConstant: 90),
+        ])
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 0.5
+        return button
     }
 }
+
+
+#Preview { ViewController() }
 
